0e87e2e4183bd0e301be7d4378547052fb17061f
Yamin Raad <yamin.raad6109@gmail.com>
2022-12-04 16:48:33 +0600

    Header commit

---
+++ src/layouts/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Shared/Footer/Footer';
+import Header from '../Components/Shared/Header/Header';
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
